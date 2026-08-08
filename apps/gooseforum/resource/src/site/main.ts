f78e1c369e00cca8dbcbf02003a6c53e89b83c83
HalfAnElephant <12142917@qq.com>
2026-08-08 13:21:01 +0800

    feat: wire appearance settings and click ripple at site startup

--- apps/gooseforum/resource/src/site/main.ts
+++ apps/gooseforum/resource/src/site/main.ts
@@ -6,6 +6,8 @@ import { installNavigation, preparePayload } from '@/runtime/router'
 import { currentLocale, i18n } from '@/runtime/i18n'
 import { hydrateFlashMessages } from '@/runtime/flash-message'
 import { applySiteThemePayload, applyStoredTheme } from '@/runtime/site-theme'
+import { applyStoredAppearanceSettings } from '@/runtime/appearance-settings'
+import { installClickRipple } from '@/runtime/click-ripple'
 import PayloadRouteView from '@/site/components/PayloadRouteView.vue'
 import { codeHighlightDirective } from '@/runtime/code-highlight-directive'
 import { mathRenderDirective } from '@/runtime/math-render-directive'
@@ -21,6 +23,8 @@ const isReloadNavigation = navigationEntry?.type === 'reload'
 document.documentElement.lang = currentLocale()
 applySiteThemePayload(initialPayload.layout.theme)
 applyStoredTheme()
+applyStoredAppearanceSettings()
+installClickRipple()
 
 function commitPage(nextPage: typeof initialPage) {
   currentPage.value = nextPage
