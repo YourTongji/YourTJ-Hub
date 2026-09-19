ede49b69761b1052b9f8110faeb04a03272fc5cc
HalfAnElephant <12142917@qq.com>
2026-09-19 22:53:34 +0800

    fix: prevent SaveUser credential rollback paths

--- apps/gooseforum/app/http/controllers/api/userController.go
+++ apps/gooseforum/app/http/controllers/api/userController.go
@@ -249,8 +249,7 @@ func EditUsername(req component.BetterRequest[EditUsernameReq]) component.Respon
 	if users.ExistUsername(newUsername) {
 		return component.FailResponseCode(component.MessageAuthUsernameExists, nil)
 	}
-	userEntity.Username = newUsername
-	err = userservice.SaveUser(&userEntity)
+	err = userservice.UpdateUserFields(userEntity.Id, map[string]any{"username": newUsername})
 	if err != nil {
 		return component.FailResponseCode(component.MessageUserUpdateFailed, nil)
 	}
@@ -312,7 +311,15 @@ func EditUserInfo(req component.BetterRequest[EditUserInfoReq]) component.Respon
 	}
 	userEntity.ExternalInformation = req.Params.ExternalInformation
 
-	err = userservice.SaveUser(&userEntity)
+	err = userservice.UpdateUserFields(userEntity.Id, map[string]any{
+		"nickname":             userEntity.Nickname,
+		"bio":                  userEntity.Bio,
+		"signature":            userEntity.Signature,
+		"website":              userEntity.Website,
+		"website_name":         userEntity.WebsiteName,
+		"locale":               userEntity.Locale,
+		"external_information": userEntity.ExternalInformation,
+	})
 	if err != nil {
 		return component.FailResponseCode(component.MessageUserUpdateFailed, nil)
 	}
@@ -332,7 +339,7 @@ func EditUserProfileCover(req component.BetterRequest[EditUserProfileCoverReq]) 
 	}
 
 	userEntity.ProfileCoverUrl = strings.TrimSpace(req.Params.ProfileCoverUrl)
-	err = userservice.SaveUser(&userEntity)
+	err = userservice.UpdateUserFields(userEntity.Id, map[string]any{"profile_cover_url": userEntity.ProfileCoverUrl})
 	if err != nil {
 		return component.FailResponseCode(component.MessageUserUpdateFailed, nil)
 	}
@@ -356,7 +363,7 @@ func SetPresetAvatar(req component.BetterRequest[SetPresetAvatarReq]) component.
 	}
 
 	userEntity.AvatarUrl = avatarUrl
-	if err := userservice.SaveUser(&userEntity); err != nil {
+	if err := userservice.UpdateUserFields(userEntity.Id, map[string]any{"avatar_url": avatarUrl}); err != nil {
 		return component.FailResponseCode(component.MessageUserUpdateFailed, nil)
 	}
 	return component.SuccessResponse(map[string]string{
@@ -493,7 +500,7 @@ func UploadAvatar(c *gin.Context) {
 	}
 
 	userEntity.AvatarUrl = fileEntities[0].Name
-	if err := userservice.SaveUser(&userEntity); err != nil {
+	if err := userservice.UpdateUserFields(userEntity.Id, map[string]any{"avatar_url": userEntity.AvatarUrl}); err != nil {
 		c.JSON(200, component.FailDataCode(component.MessageUserUpdateFailed, nil))
 		return
 	}
